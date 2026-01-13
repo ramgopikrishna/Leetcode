bffe3d0f328ee49281dc5840467312bd452da15f
Gopi Krishna <gkrishna@lantronix.com>
2026-01-13 11:01:09 +0530

    Update rom_to_int

--- rom_to_int.c
+++ rom_to_int.c
@@ -1,62 +1,112 @@
+/* Convert Roman to Int */
+/*
+Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+
+Symbol       Value
+I             1
+V             5
+X             10
+L             50
+C             100
+D             500
+M             1000
+
+For example, 2 is written as II in Roman numeral, just two ones added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+
+Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+
+    I can be placed before V (5) and X (10) to make 4 and 9. 
+    X can be placed before L (50) and C (100) to make 40 and 90. 
+    C can be placed before D (500) and M (1000) to make 400 and 900.
+
+Given a roman numeral, convert it to an integer.
+
+Example 1:
+
+Input: s = "III"
+Output: 3
+Explanation: III = 3.
+
+Example 2:
+
+Input: s = "LVIII"
+Output: 58
+Explanation: L = 50, V= 5, III = 3.
+
+Example 3:
+
+Input: s = "MCMXCIV"
+Output: 1994
+Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+
+Constraints:
+
+    1 <= s.length <= 15
+    s contains only the characters ('I', 'V', 'X', 'L', 'C', 'D', 'M').
+    It is guaranteed that s is a valid roman numeral in the range [1, 3999].
+
+*/
+
 #include <stdio.h>
 #include <string.h>
 
-
 int romanToInt(char* s)
 {
-    int len=strlen(s);
-    int final_value = 0;
-    int last_value = 0;
-    int tmp = 0;
+	int i;
+	int final_value;
+	int last_value;
+	int tmp;
+	int len=strlen(s);
 
-    int i;
-    for(i=0; i<len; i++)
-    {
-    	if(s[i] == 'M')
+	for(i=0; i<len; i++)
 	{
-	    tmp=1000;
-	}
-    	if(s[i] == 'D')
-	{
-	    tmp=500;
-	}
-    	if(s[i] == 'C')
-	{
-	    tmp=100;
-	}
-    	if(s[i] == 'L')
-	{
-	    tmp=50;
-	}
-    	if(s[i] == 'X')
-	{
-	    tmp=10;
-	}
-    	if(s[i] == 'V')
-	{
-	    tmp=5;
-	}
-    	if(s[i] == 'I')
-	{
-	    tmp=1;
-	}
-	final_value +=tmp;
+		if(s[i] == 'M')
+		{
+			tmp=1000;
+		}
+		if(s[i] == 'D')
+		{
+			tmp=500;
+		}
+		if(s[i] == 'C')
+		{
+			tmp=100;
+		}
+		if(s[i] == 'L')
+		{
+			tmp=50;
+		}
+		if(s[i] == 'X')
+		{
+			tmp=10;
+		}
+		if(s[i] == 'V')
+		{
+			tmp=5;
+		}
+		if(s[i] == 'I')
+		{
+			tmp=1;
+		}
+		final_value +=tmp;
 
-	if(i>0 && last_value<tmp)
-	{
-	    final_value=final_value-(last_value*2);
+		if(i>0 && last_value<tmp)
+		{
+			final_value=final_value-(last_value*2);
+		}
+		last_value=tmp;
 	}
-	last_value=tmp;
-    }
-    return final_value;
+	return final_value;
 }
 
 int main(void)
 {
-    int s = romanToInt("MCMXCIV");
-    printf("RAMA %d\n", s);
-    s = romanToInt("LVIII");
-    printf("RAMA %d\n", s);
-    s = romanToInt("III");
-    printf("RAMA %d\n", s);
+	int s = romanToInt("MCMXCIV");
+	printf("RAMA %d\n", s);
+
+	s = romanToInt("LVIII");
+	printf("RAMA %d\n", s);
+
+	s = romanToInt("III");
+	printf("RAMA %d\n", s);
 }
